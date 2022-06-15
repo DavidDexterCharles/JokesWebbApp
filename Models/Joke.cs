b98d59725a4e4d7c4cd53d6d20ec3ce4149916d2
David <david.dexter.charles@gmail.com>
2022-06-15 19:45:19 -0400

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace JokesWebbApp.Models
+{
+    public partial class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
